127aa01fbc7bc755bca2b070814a7dc4aa010864
derphpeter <57673964+derphpeter@users.noreply.github.com>
2023-05-06 11:56:38 +0200

    Added Data Connector und Data Models
    Connection String fehlt -> müsste noch jeder für sich anlegen, siehe https://www.connectionstrings.com/

---
+++ EinkaufApp/EinkaufApp/Models/ArtikelModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EinkaufApp.Models
+{
+    public class ArtikelModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Kind { get; set; }
+        public int QuantitiyIs { get; set; }
+        public int QuanitityShall { get; set; }
+        public int QuantityIncoming { get; set; }
+        public int QuantityOutgoing { get; set; }
+        public string Lieferant { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ EinkaufApp/EinkaufApp/Models/ArtikelartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EinkaufApp.Models
+{
+    public class ArtikelartModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
